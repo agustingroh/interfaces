5c06e0eee6414668abcbe55caed782be359dd86b
Agustín Groh <laragiarrocco@gmail.com>
2022-10-02 00:56:04 -0300

    adds carrousels on home page

---
+++ entrega2/js/home.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function (){
+document.querySelector("#home-btn-carrousel-latest-relese-fordward").addEventListener('click',()=>{
+   const container = document.querySelector("#home-latest-releases-container");
+   container.classList.add("translated");
+});
+
+});
+
