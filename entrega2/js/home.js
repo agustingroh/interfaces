445814ff369121f25c6e2b9b41d719699e88e70b
laragiar <90468640+laragiar@users.noreply.github.com>
2022-10-07 17:52:05 -0300

    Add carousels on home page
    * adds carrousels on home page
    
    * Adds-carrousels-category
    
    * Adds-carrousels-on-main-page
    
    Co-authored-by: Agustin Groh <agusgroh@gmail.com>

---
+++ entrega2/js/home.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function (){
+document.querySelector("#home-btn-carrousel-fordward").addEventListener('click',()=>{
+   const container = document.querySelector("#home-carrousel-container");
+   container.classList.add("translated");
+});
+
+});
+
