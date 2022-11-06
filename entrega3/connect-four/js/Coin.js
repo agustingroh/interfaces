5e47e6a762429a158d42b38c897507f2d2da9e01
Agustin Groh <agusgroh@gmail.com>
2022-11-05 21:03:08 -0300

    Fixes bug on coin background image

--- entrega3/connect-four/js/Coin.js
+++ entrega3/connect-four/js/Coin.js
@@ -17,7 +17,6 @@ class Coin {
         this.y =  null;
         this.radio = radio;
         this.image = null;
-        this.isImgLoaded = false;
     }
 
 
@@ -46,16 +45,12 @@ class Coin {
     }
 
    async loadImage(){
-    console.log(this.index);
-    return new Promise((resolve,reject)=>{
-        this.image.onload = () => {
-            this.ctx.drawImage(this.image,this.x,this.y,this.getRadio(),this.getRadio());
-            console.log(this.index);
-            resolve(true);
-        }
-    });
-      
-       
+        return new Promise((resolve,reject)=>{
+            this.image.onload = () => {
+                this.ctx.drawImage(this.image,this.x,this.y,this.getRadio(),this.getRadio());
+                resolve(true);
+            }
+        });
     }
 
     setInitX(x){
@@ -97,37 +92,17 @@ class Coin {
                 resolve(true);
             });
            this.image.src = this.color;
-           
         });
-}
-
-
-    draw(stroke){        
-        this.ctx.beginPath();
-        this.ctx.arc(this.x,this.y ,this.getRadio(),0,2*Math.PI); 
-        this.ctx.fill();
-        this.ctx.closePath();
-        this.ctx.drawImage(this.image,this.x,this.y,this.getRadio(),this.getRadio());
-        this.image.src = this.color;
-
-       /*  if(stroke) {
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.getRadio(), 0, 2 * Math.PI);
-            ctx.strokeStyle = "#000b23";
-            ctx.stroke();
-        }
-       
-        ctx.fillStyle = this.color;
-        console.log("value",this.color);
-        this.ctx.beginPath();
-        this.ctx.arc(this.x,this.y ,this.getRadio(),0,2*Math.PI);  */  
-        // this.ctx.fill();
-        //this.image.src = 'C:/Users/Lara/Desktop/Lara/Interfaces/entrega3/img/coin2.png';
-
     }
 
-  
-    
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.getRadio(), 0, 2 * Math.PI);
+        this.ctx.fill();
+        this.ctx.closePath();
+        this.ctx.drawImage(this.image,(this.x - this.getRadio()),(this.y - this.getRadio()),this.getRadio() * 2,this.getRadio() * 2);
+        this.image.src = this.color;
+    }
    
     isSelected(x,y){
         let aux = (this.x - x) * (this.x - x) + (this.y - y) * (this.y - y);
@@ -136,6 +111,5 @@ class Coin {
         if(dist < r) return true;
         return false;
     }
-
 
 }
